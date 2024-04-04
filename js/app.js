51142e4d1cc0dda04e7405ba6e1010c80271ba82
Seth-Kimani <sethkimani99@gmail.com>
2024-04-04 19:55:04 +0300

    transform

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const parallax_el = document.querySelectorAll(".parallax");
+
+let xVal, yVal = 0;
+
+window.addEventListener("mousemove", (e) => {
+    xVal = e.clientX - window.innerWidth / 2;
+    yVal = e.clientY - window.innerHeight / 2;
+    // console.log(xVal,yVal);
+    parallax_el.forEach((el) => {
+
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+
+        el.style.transform = `translateX(calc(-50% + ${-xVal * speedx}px)) translateY(calc(-50% + ${-yVal * speedy}px))`;
+    });
+});
